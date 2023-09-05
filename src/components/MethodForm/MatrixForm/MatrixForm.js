fc63ffd54cce024fc6a9996bfe45145b33bad969
Mohammed Shady Elsayed <75118102+mohammedshady@users.noreply.github.com>
2023-09-05 16:29:12 +0300

    added Amoingus

--- src/components/MethodForm/MatrixForm/MatrixForm.js
+++ src/components/MethodForm/MatrixForm/MatrixForm.js
@@ -1,51 +1,39 @@
-import "./MatrixForm.css"
+import "./MatrixForm.css";
 import React, { useState } from "react";
 import { useEffect } from "react";
 
 const MatrixForm = ({ initialValues }) => {
+  const [inputValues, setInputValues] = useState(initialValues);
+  useEffect(() => {
+    setInputValues(initialValues);
+  }, [initialValues]);
 
-    const [inputValues, setInputValues] = useState(initialValues);
-    useEffect(() => {
-        setInputValues(initialValues);
+  // AMONGUSS
 
-    }, [initialValues])
+  console.log(inputValues);
 
+  return (
+    <>
+      <form className="matrix-form">
+        {inputValues.matrix.map((row, index) => {
+          return (
+            <div key={row[0].name}>
+              {row.map((input, index) => {
+                return (
+                  <input
+                    key={input.name}
+                    className="matrix-input"
+                    name={input.name}
+                    placeholder={input.name}
+                  ></input>
+                );
+              })}
+            </div>
+          );
+        })}
+      </form>
+    </>
+  );
+};
 
-
-
-
-
-
-
-
-    console.log(inputValues);
-
-
-
-
-
-    return (
-        <>
-            <form className="matrix-form">
-                {inputValues.matrix.map((row, index) => {
-                    return (
-                        <div key={row[0].name}>{
-                            row.map((input, index) => {
-                                return (
-                                    <input key={input.name} className="matrix-input" name={input.name} placeholder={input.name}></input>
-                                )
-                            })
-                        }
-                        </div>
-                    )
-                })
-                }
-
-
-
-            </form >
-        </>
-    )
-}
-
-export default MatrixForm
+export default MatrixForm;
